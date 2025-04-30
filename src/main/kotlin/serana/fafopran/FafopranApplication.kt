64db4f5696c6a7d6000c98c77f0193231bd50836
PiksaevaAY <piksaevaay@skblab.ru>
2025-04-30 11:13:57 +0500

    init

---
+++ src/main/kotlin/serana/fafopran/FafopranApplication.kt
@@ -0,0 +1,11 @@
+package serana.fafopran
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FafopranApplication
+
+fun main(args: Array<String>) {
+	runApplication<FafopranApplication>(*args)
+}
---
+++ src/test/kotlin/serana/fafopran/FafopranApplicationTests.kt
@@ -0,0 +1,13 @@
+package serana.fafopran
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FafopranApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
